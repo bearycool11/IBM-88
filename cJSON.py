e12d0c788da59f2efa5e93781750d9e845064b8d
J. K. Edwards <joed6834@colorado.edu>
2025-03-11 12:27:53 -0400

    Update cJSON.py

--- cJSON.py
+++ cJSON.py
@@ -1,6 +1,41 @@
 import ctypes
 import json
 
+class CJSON:
+    def __init__(self, lib_path):
+        self.lib = ctypes.CDLL(lib_path)
+        self.lib.json_parse.argtypes = [ctypes.c_char_p]
+        self.lib.json_parse.restype = ctypes.c_void_p
+        self.lib.json_stringify.argtypes = [ctypes.c_void_p]
+        self.lib.json_stringify.restype = ctypes.c_char_p
+        self.lib.json_get_string.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
+        self.lib.json_get_string.restype = ctypes.c_char_p
+
+    def parse(self, json_str):
+        return self.lib.json_parse(json_str.encode())
+
+    def stringify(self, json_obj):
+        return self.lib.json_stringify(json_obj).decode()
+
+    def get_string(self, json_obj, key):
+        return self.lib.json_get_string(json_obj, key.encode()).decode()
+
+def main():
+    lib_path = './json.so'
+    cjson = CJSON(lib_path)
+
+    with open('vocab.json', 'r') as file:
+        json_str = file.read()
+
+    json_obj = cjson.parse(json_str)
+    json_str2 = cjson.stringify(json_obj)
+    dict = json.loads(json_str2)
+    value = dict.get('key')
+    print(value)
+
+if __name__ == "__main__":
+    main()
+
 # Load the shared library
 lib = ctypes.CDLL('./json.so')
 
